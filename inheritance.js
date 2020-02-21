8f8f88d342708bc5c9768297eef47c9ea5382608
Aaron Leu <aaronaleu@gmail.com>
2020-02-21 15:04:44 -0500

    Feature: Added inheritance.js

---
+++ inheritance.js
@@ -0,0 +1,13 @@
+class Animal {
+    move(steps) {
+        return 'moved ' + steps + ' steps';
+    }
+}
+class Dog extends Animal {
+    bark() {
+        return 'Bark!';
+    }
+}
+let doggo = new Dog();
+console.log('Doggo ' + doggo.move(5)); //Dog inherits move method from the Animal class
+console.log(doggo.bark());
